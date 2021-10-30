1047c46a19e3940f49976a1deee45637fef74f93
kodanrag <raghavendra.kodandarama@gmail.com>
2021-10-30 16:05:54 +0530

    Added terraform backend block

--- tf_ec2_linux_ex/main.tf
+++ tf_ec2_linux_ex/main.tf
@@ -2,9 +2,27 @@
 
 provider "aws" {
 	region 	= "us-east-1"
+	aws_access_key_id = var.aws_access_key_id
+	aws_secret_access_key = var.aws_secret_access_key
 }
 
 resource "aws_instance" "tf_lab2_instance" {
 	ami 		= "ami-02e136e904f3da870"
 	instance_type 	= "t2.micro"
+}
+
+
+# The below code is used to enable remote execution. Currently we are using this code to run from Terraform cloud. 
+# Ref URL: ---> https://www.terraform.io/docs/language/settings/backends/remote.html#basic-configuration
+
+# Using a single workspace:
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "devopsrk"
+
+    workspaces {
+      name = "terraform-learning"
+    }
+  }
 }
